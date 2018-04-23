d07cf25ff370562070fa797427a771d4251ede7d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DevExpress.Data.Filtering;
+using DevExpress.Web.ASPxGridView;
+
+
+
+public partial class _Default : System.Web.UI.Page {
+    private string GetFilterExpression() {
+        string res_str = string.Empty;
+        List<CriteriaOperator> lst_operator = new List<CriteriaOperator>();
+
+        if (!string.IsNullOrEmpty(txtFirstName.Text))
+            lst_operator.Add(new BinaryOperator("FirstName", string.Format("%{0}%", txtFirstName.Text), BinaryOperatorType.Like));
+
+        if (!string.IsNullOrEmpty(txtLastName.Text))
+            lst_operator.Add(new BinaryOperator("LastName", string.Format("%{0}%", txtLastName.Text), BinaryOperatorType.Like));
+
+        if (deHDFrom.Value != null)
+            lst_operator.Add(new BinaryOperator("HireDate", deHDFrom.Date, BinaryOperatorType.GreaterOrEqual));
+
+        if (deHDTo.Value != null)
+            lst_operator.Add(new BinaryOperator("HireDate", deHDTo.Date, BinaryOperatorType.LessOrEqual));
+
+        if (lst_operator.Count > 0) {
+            CriteriaOperator[] op = new CriteriaOperator[lst_operator.Count];
+            for (int i = 0; i < lst_operator.Count; i++)
+                op[i] = lst_operator[i];
+            CriteriaOperator res_operator = new GroupOperator(GroupOperatorType.And, op);
+            res_str = res_operator.ToString();
+        }
+
+        return res_str;
+    }
+    protected void gvData_DataBinding(object sender, EventArgs e) {
+        string f_expression = GetFilterExpression();
+
+        if (f_expression == string.Empty)
+            gvData.DataSource = null;
+        else
+            gvData.DataSource = adsData;
+
+        gvData.FilterExpression = f_expression;
+    }
+    protected void gvData_CustomCallback(object sender, ASPxGridViewCustomCallbackEventArgs e) {
+        gvData.DataBind();
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to implement an external filter for the ASPxGridView (ASPxGridView binds via its callback)
+
+
+<p><strong>UPDATED:<br /></strong><br />Starting with version 14.2, GridView provides the built-in Search / Find Panel functionality with the capability to locate it outside grid boundaries. This allows accomplishing a similar task with less effort and does not require so much extra code. See the <a href="https://community.devexpress.com/blogs/aspnet/archive/2014/11/19/asp-net-data-grid-enhancements-coming-soon-in-v14-2.aspx">ASP.NET Data Grid: Enhancements</a> post to learn more about this new functionality.<br /><br />The example demonstrates how to implement an external filter for the ASPxGridView control. To accomplish this, handle the <strong>ASPxGridView.DataBinding</strong> event and specify a value of the <strong>ASPxGridView.FilterExpression</strong> property by using the BinaryOperator/GroupOperator types based on values of external editors. To rebind the grid, we perform a callback via the <strong>ASPxClientGridView.PerformCallback</strong> method.</p>
+<p><strong>See also: </strong><br /> <a href="https://www.devexpress.com/Support/Center/p/E3748">How to implement an external filter for the ASPxGridView (ASPxGridView binds via postback)</a><br /> <a href="https://www.devexpress.com/Support/Center/p/E2041">How to filter ASPxGridView bound to SqlDataSource by using an external ASPxComboBox in Template</a></p>
+
+<br/>
+
+
